249880c601efe68f027e0c4228156fccd764feb3
yangliu <liu00633@algonquinlive.com>
2025-02-17 12:04:27 -0500

    complete the assignment

--- t-shirts.js
+++ t-shirts.js
@@ -63,3 +63,89 @@ const tshirts = [
     quantity: 1
   }
 ]
+
+
+function Title(props) {
+  return <h2>{props.title}</h2>
+}
+
+function Image(props) {
+  return <img className="img-fluid" src={'images/' + props.src} alt={props.title} />
+}
+
+function Stock({stock}) {
+  const style = {
+    color: stock < 2 ? 'red' : 'black'
+  }
+  return stock ? <p className="transition" style={style}>{stock} left</p> : <p className="animate">Sold Out!</p>
+}
+
+const Price = ({price}) => {
+  return <p><strong>${price}</strong></p> 
+}
+
+function Buy ({ stock, onBuy, quantity, onQtyChange }) {
+  function clickHandler(e) {
+    e.preventDefault();
+    onBuy();
+  }
+
+  return (
+    stock ? 
+    <form>
+      <div className="input-group mb-3">
+        <input type="number" className="form-control" value={quantity} placeholder="0"  min="1" max={stock} onChange={(e) => onQtyChange(e.target.value)} />
+        <button type="submit" className="btn btn-outline-secondary" onClick={(e) => clickHandler(e)} >Buy</button>
+      </div>
+    </form> : ''
+  )
+}
+
+function Tshirt({tshirt}) {
+  const [stock, setStock] = React.useState(tshirt.stock)
+  const [quantity, setQuantity] = React.useState(tshirt.quantity)
+  
+  function buyHandler() {
+    if(stock >= quantity) {
+      setStock(stock - quantity)
+    }
+  }
+
+  function qtyChangeHandler(value){
+    setQuantity(value)
+  }
+
+  return (
+    <div className="col col-12 col-md-6 col-lg-4 my-3">
+      <Image src={tshirt.image} title={tshirt.title}/>
+      <Title title={tshirt.title} />
+      <Price price={tshirt.price} />
+      <Stock stock={stock} />
+
+      <div className="row">
+        <div className="col-4">
+          <Buy stock={stock} quantity={quantity} onBuy={buyHandler} onQtyChange={qtyChangeHandler} />
+        </div>
+      </div>
+    </div>
+  )
+} 
+
+function App () {
+  return ( 
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <h1>T-Shirts</h1>
+        </div>
+      </div>
+      <div className="row">
+        {tshirts.map(tshirt => <Tshirt key={tshirt.id} tshirt={tshirt} />)}
+      </div>
+    </div>
+  )
+}
+
+
+const root = ReactDOM.createRoot(document.querySelector('#root'))
+root.render(<App />)
